07dab2fdf23a87d05d43d948a516e810ee803425
Teguh Permana <teguhpermana096@gmail.com>
2023-08-09 23:30:51 +0800

    [Settings] #1 Setup environment path

--- settings.py
+++ settings.py
@@ -6,3 +6,48 @@ file_path = Path(__file__).resolve()
 
 # Get the parent directory of the current file
 root_path = file_path.parent
+
+# Add the root path to the sys.path list if it is not already there
+if root_path not in sys.path:
+    sys.path.append(str(root_path))
+
+# Get the realtive path of the root directory with respect 
+ROOT = root_path.relative_to(Path.cwd())
+
+# Sources 
+IMAGE   = "Image"
+VIDEO   = "Video"
+WEBCAM  = "Webcam"
+RTSP    = "RTSP"
+YOUTUBE = "YouTube"
+
+SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, YOUTUBE]
+
+# Image config
+IMAGES_DIR = ROOT/"images" 
+DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / "office_4_detected.jpg"
+
+# Video Config
+VIDEO_DIR = ROOT/ "videos"
+VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
+VIDEO_2_PATH = VIDEO_DIR / "video_2.mp4"
+VIDEO_3_PATH = VIDEO_DIR / "video_3.mp4"
+VIDEO_4_PATH = VIDEO_DIR / "video_4.mp4"
+VIDEO_5_PATH = VIDEO_DIR / "video_5.mp4"
+
+VIDEO_DICT = {
+    "video_1" : VIDEO_1_PATH,
+    "video_2" : VIDEO_2_PATH,
+    "video_3" : VIDEO_3_PATH,
+    "video_4" : VIDEO_4_PATH,
+    "video_5" : VIDEO_5_PATH
+}
+
+# ML Model Config
+MODEL_DIR = ROOT/"weights"
+DETECTION_MODEL = MODEL_DIR/"yolov8n.pt"
+SEGMENTATION_MODEL = MODEL_DIR/"yolov8n-seg.pt"
+
+# Webcam
+WEBCAM_PATH = 0
